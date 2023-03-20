774ea7362c633561f89ed3d29389a680fe293f3c
jwang <jiapan.wang119@gmail.com>
2023-03-20 11:04:30 +0100

    update template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Readthedocs Template'
+copyright = '2023, Jiapan Wang'
+author = 'Jiapan Wang'
 
 release = '0.1'
 version = '0.1.0'
